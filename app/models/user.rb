0374ef69c23a09b173a4027c6ee202f018d99ce2
nurarenke <nurarenke@gmail.com>
2019-06-08 09:03:11 -0700

    Validation For Password Length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   validates :name, presence: true
   validates :email, presence: true,
                   format: /\A\S+@\S+\z/,
                   uniqueness: { case_sensitive: false }
-  has_secure_password
+  validates :password, length: { minimum: 8, allow_blank: true }
 end
