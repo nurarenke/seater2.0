1eb513caaf8e43ee0d727b40b0824ee3b1a1d5e6
nurarenke <nurarenke@gmail.com>
2019-06-23 17:52:55 -0700

    New User Page and Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 end
