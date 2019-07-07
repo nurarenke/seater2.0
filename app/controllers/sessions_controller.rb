02811526417582de19d2dc895d07996120f321d6
nurarenke <nurarenke@gmail.com>
2019-07-07 15:19:52 -0700

    Sign In Finished and User Model Tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Your password or emaild do not match. Please try again."
+      render :new
   end
+end
 end
