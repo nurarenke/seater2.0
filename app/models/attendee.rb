94b966f8593cc48359bd5933dd03ab222ed588e0
nurarenke <nurarenke@gmail.com>
2019-08-01 12:56:57 -0700

    Delete wrong attendee and table models

--- app/models/attendee.rb
+++
@@ -1,4 +0,0 @@
-class Attendee < ApplicationRecord
-  belongs_to :event
-  belongs_to :table
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20190430035939) do
 
-  create_table "attendees", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "event_id"
-    t.integer  "table_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_attendees_on_event_id"
-    t.index ["table_id"], name: "index_attendees_on_table_id"
-  end
-
   create_table "events", force: :cascade do |t|
     t.string   "event_name"
     t.string   "event_description"
@@ -32,15 +21,6 @@ ActiveRecord::Schema.define(version: 20190430035939) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.index ["user_id"], name: "index_events_on_user_id"
-  end
-
-  create_table "tables", force: :cascade do |t|
-    t.string   "table_name"
-    t.integer  "max_seats"
-    t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_tables_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
