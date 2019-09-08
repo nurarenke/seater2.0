7254f6e513c82b422395e3323051a552bc93c291
nurarenke <nurarenke@gmail.com>
2019-09-08 08:48:55 -0700

    Create Action and New Action for Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,9 +3,33 @@ class EventsController < ApplicationController
     @events = Event.where(user_id: session[:user_id])
   end
 
+  def new
+    @event = Event.new
+    @current_user = User.find(session[:user_id])
+  end
+
+  def create
+    @current_user = User.find(session[:user_id])
+    @event = Event.create(event_params.merge(user_id: @current_user.id))
+    if @event.save
+      flash[:notice] = "Event was successfully created"
+      redirect_to user_events_path(@current_user)
+    else
+      flash[:notice] = "Event was not successfully created"
+      render :new
+    end
+
+  end
+
   def show
     @event = Event.find(params[:id])
     @attendees = Attendee.where(event_id: params[:id])
     @tables = Table.where(event_id:params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:event_name, :event_description, :location, :date)
   end
 end
